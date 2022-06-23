556451749dd89f0eeb6508cbbf0a15ac933e3fa4
Alexandre Meline <meline.alexandre@laposte.net>
2022-06-23 09:32:09 +0200

    First commit

---
+++ db/migrate/20220623072522_create_cars.rb
@@ -0,0 +1,13 @@
+class CreateCars < ActiveRecord::Migration[6.1]
+  def change
+    create_table :cars do |t|
+      t.string :model
+      t.date :model_date
+      t.integer :km_price
+      t.integer :day_price
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
